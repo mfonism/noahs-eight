1d2fae1f2fccc9b8fa6b3b6a8f1831d96466b364
Mfon Eti-mfon <mfonetimfon@gmail.com>
2020-02-04 12:46:47 +0100

    display an image given its file path

---
+++ image_display_utils.py
@@ -0,0 +1,17 @@
+from PIL import Image
+
+
+def display_image(image_file_path):
+    """
+    Display image which exists at argument file path.
+    """
+    image_pil = Image.open(image_file_path)
+    image_pil.show()
+
+
+if __name__ == "__main__":
+    import constants
+    import utils
+
+    for image_file_path in utils.deep_iterdir(constants.UNKNOWN_FACES_PATH):
+        display_image(image_file_path)
